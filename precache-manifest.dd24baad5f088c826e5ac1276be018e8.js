5f4d78e5a8d3f8b16defaf847a9e6e3a79d94fd6
aviveldan <aviveldan@gmail.com>
2020-04-15 16:12:10 +0300

    Updates

---
+++ precache-manifest.dd24baad5f088c826e5ac1276be018e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba403027a2f92a89869c6a6854df043",
+    "url": "/yoyo_motion/index.html"
+  },
+  {
+    "revision": "b238ad853bcb2727f276",
+    "url": "/yoyo_motion/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "1fc55c46ab0fdb8a93b7",
+    "url": "/yoyo_motion/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b238ad853bcb2727f276",
+    "url": "/yoyo_motion/static/js/2.fd40f9fd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yoyo_motion/static/js/2.fd40f9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc55c46ab0fdb8a93b7",
+    "url": "/yoyo_motion/static/js/main.b59fd948.chunk.js"
+  },
+  {
+    "revision": "69465412c20acb34e964",
+    "url": "/yoyo_motion/static/js/runtime-main.e47cffe9.js"
+  },
+  {
+    "revision": "7f765d2266d5e8ca25731879875e3cd3",
+    "url": "/yoyo_motion/static/media/airbnb.7f765d22.jfif"
+  },
+  {
+    "revision": "47d219ea559114afb12af280eb137e69",
+    "url": "/yoyo_motion/static/media/icq.47d219ea.jfif"
+  },
+  {
+    "revision": "bae79365e5456e7d29520066f3d2952b",
+    "url": "/yoyo_motion/static/media/logo.bae79365.png"
+  },
+  {
+    "revision": "2bd174d7828624e6986bf19c1da2e0d6",
+    "url": "/yoyo_motion/static/media/yehonatan.2bd174d7.jpg"
+  }
+]);
